8b1f1173c33d802deabbc97be11f0ab32d227d90
GrowFatLikeMe <freakishcrap@gmail.com>
2025-07-31 17:15:00 +0800

    Initial commit: Add Kubernetes and Terraform configurations

---
+++ terraform/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.4"
+
+    }
+  }
+
+  backend "s3" {
+    bucket = "sctp-ce10-tfstate"
+    key    = "growfat-k8slab.tfstate"
+    region = "ap-southeast-1"
+  }
+}
